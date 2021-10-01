415288b9231b6279f4bead8ded9e898ea15b9c8a
kariakito <pilarmaestre@gmail.com>
2021-10-01 10:10:53 +0200

    actualitation of js carousel

--- js/carousel.js
+++ js/carousel.js
@@ -5,12 +5,23 @@ const prevButton = document.querySelector('.carousel_button--left');
 const dotsNav = document.querySelector('.carusel_nav');
 const dots = Array.from(dotsNav.children);
 
-const slideSize = slides[0].getBoundingClientRect(); 
+const slideSize = slides[0].getBoundingClientRect().width; 
 
-console.log(slideSize)
+//console.log(slideSize)
 
+// slides[0].style.left = slideWidth * 0 + 'px';
+// slides[1].style.left = slideWidth * 1 + 'px';
+// slides[2].style.left = slideWidth * 2 + 'px';
+// slides[3].style.left = slideWidth * 3 + 'px';
+// slides[4].style.left = slideWidth * 4 + 'px';
 
-// click left, clicl rigth, nav indicator move to the slide
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+}
+
+slide.forEach(setSlidePosition);
+
+// click left, clicl rigth, nav indicator move to the slide,arrage slide next to one another
 
 
 
